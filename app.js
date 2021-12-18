9506739889ae8426b914174e18271d78575081fc
abdulwasaykhann <abdulwasaywasaykhan8@gmail.com>
2021-12-18 14:48:04 +0500

    assignment

---
+++ app.js
@@ -0,0 +1,92 @@
+// --- Changing Case___ch#21---
+// tolowercase
+
+// var cityToCheck = prompt("Enter your city");
+// cityToCheck = cityToCheck.toLowerCase();
+// var cleanestCities = ["india", "pakistan", "amarica", "turkey", "iran"];
+// for (var i = 0; i <= 4; i++) {
+// if (cityToCheck === cleanestCities[i]) {
+// alert("It's one of the cleanest cities");
+// }
+// }
+//----------------------------------
+//  toUppercase
+
+// var cityToCheck = prompt("Enter your city");
+// cityToCheck = cityToCheck.toUpperCase();
+// var cleanestCities = ["india", "pakistan", "amarica", "turkey", "iran"];
+// for (var i = 0; i <= 4; i++) {
+// if (cityToCheck === cleanestCities[i]) {
+// alert("It's one of the cleanest cities");
+// }
+// }
+
+
+
+
+
+//--- Strings: Measuring length and extracting parts___ch#22---
+// var str = prompt("Enter some text");
+// var numChars = str.length;
+// for (var i = 0; i < numChars; i++) {
+// if (str.slice(i, i + 2) === " ") {
+// alert("No double spaces!");
+// break;
+//     }
+// }
+
+
+
+
+
+//--- Strings: Finding segments___ch#23---
+// var firstChar = text.indexOf("World War II");
+// if (firstChar !== -1) {
+// text = text.slice(0, firstChar) + "the Second World War" + text.slice(firstChar + 12);
+// {
+//     var text = "To be or not to be.";
+//     var segIndex = text.lastIndexOf("be");
+// }
+
+
+
+
+
+//---    ___ch#24---
+// for (var i = 0; i < text.length; i++) {
+//     if (text.charAt(i) === "!") {
+//     alert("Exclamation point found!");
+//     break;
+//     }
+// }
+
+
+
+
+//---   ___ch#25---
+// for (var i = 0; i < text.length; i++) {
+//     if (text.slice(i, i + 12) === "World War II") {
+//     text = text.slice(0, 1) + "the Second World War" + text.slice(i + 12);
+//     }
+// }
+// //---------------------------------------
+// var firstChar = text.indexOf("World War II");
+// if (firstChar !== -1) {
+//     text = text.slice(0, firstChar) + "the Second World War" + text.slice(firstChar + 12);
+// }
+
+
+
+
+
+//---   ___ch#26---
+
+
+
+
+
+
+//---   ___ch#27---
+// var randomNumber = Math.random();
+// console.log(randomNumber);
+//--------------------------
